b58d759356289b0aa3c85b592522934d9cef2d1d
Denis Kirillov <denis@nspcc.ru>
2021-09-02 16:28:07 +0300

    [#3] Add aio Dockerfile
    Signed-off-by: Denis Kirillov <denis@nspcc.ru>

--- testcontainer/init-testcontainer.sh
+++ testcontainer/init-testcontainer.sh
@@ -9,6 +9,26 @@ do
   sleep 1;
 done
 
+export NEOGO=/usr/bin/neo-go
+export WALLET=/config/node-wallet.json
+export WALLET_IMG=/config/node-wallet.json
+
+cd /config && ./bin/config.sh ContainerFee 0
+
 /bin/neofs-ir --config /config/config-ir.yaml &
 
-/bin/neofs-node --config /config/config-sn.yaml
+while [[ -z "$(/bin/neofs-cli control healthcheck --ir -r localhost:16512 --binary-key /config/wallet.key | grep 'Health status: READY')" ]];
+do
+  sleep 1;
+done
+
+set -m
+/bin/neofs-node --config /config/config-sn.yaml &
+
+while [[ -z "$(/bin/neofs-cli control healthcheck -r localhost:16513 --binary-key /config/wallet-sn.key | grep 'Health status: READY')" ]];
+do
+  sleep 1;
+done
+
+echo "aio container started"
+fg
