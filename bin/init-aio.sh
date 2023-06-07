nspcc-dev/neofs-aio

commit 74b925abde8a96ea22882d73b1ad797d30d7d5fb

[#17] Use one image
Signed-off-by: Denis Kirillov <denis@nspcc.ru>

bin/init-aio.sh
@@ -1,0 +1,37 @@
+#!/bin/bash
+
+export ACC=/config/morph_chain.gz
+
+/usr/bin/privnet-entrypoint.sh node --config-path /config --privnet &
+
+while [[ "$(curl -s -o /dev/null -w %{http_code} localhost:30333)" != "422" ]];
+do
+  sleep 2;
+done
+
+export NEOGO=/usr/bin/neo-go
+export WALLET=/config/node-wallet.json
+
+cd /config && ./bin/config.sh ContainerFee 0 && ./bin/config.sh ContainerAliasFee 0
+
+/usr/bin/neofs-ir --config /config/config-ir.yaml &
+
+while [[ -z "$(/usr/bin/neofs-cli control healthcheck --ir --endpoint localhost:16512 -c /config/cli-cfg-ir.yaml | grep 'Health status: READY')" ]];
+do
+  sleep 2;
+done
+
+set -m
+/usr/bin/neofs-node --config /config/config-sn.yaml &
+
+while [[ -z "$(/usr/bin/neofs-cli control healthcheck --endpoint localhost:16513 -c /config/cli-cfg-sn.yaml | grep 'Health status: READY')" ]];
+do
+  ./bin/tick.sh
+  sleep 5;
+done
+
+/usr/bin/neofs-http-gw &
+/usr/bin/neofs-rest-gw &
+
+echo "aio container started"
+fg

Dockerfile
@@ -1,10 +1,17 @@
-ARG HUB_IMAGE=nspccdev/neofs
-ARG HUB_TAG=latest
+ARG NEOFS_HUB_IMAGE=nspccdev/neofs
+ARG NEOFS_HUB_TAG=latest
+ARG NEOGO_HUB_IMAGE=nspccdev/neo-go
+ARG NEOGO_HUB_TAG=latest
+ARG NEOFS_HTTP_HUB_TAG=latest
+ARG NEOFS_REST_HUB_TAG=latest
 
-FROM ${HUB_IMAGE}-cli:${HUB_TAG} as neofs-cli
-FROM ${HUB_IMAGE}-ir:${HUB_TAG} as neofs-ir
-FROM ${HUB_IMAGE}-storage:${HUB_TAG} as neofs-storage
-FROM ${HUB_IMAGE}-adm:${HUB_TAG} as neofs-adm
+FROM ${NEOGO_HUB_IMAGE}:${NEOGO_HUB_TAG} as neo-go
+FROM ${NEOFS_HUB_IMAGE}-cli:${NEOFS_HUB_TAG} as neofs-cli
+FROM ${NEOFS_HUB_IMAGE}-ir:${NEOFS_HUB_TAG} as neofs-ir
+FROM ${NEOFS_HUB_IMAGE}-storage:${NEOFS_HUB_TAG} as neofs-storage
+FROM ${NEOFS_HUB_IMAGE}-adm:${NEOFS_HUB_TAG} as neofs-adm
+FROM ${NEOFS_HUB_IMAGE}-http-gw:${NEOFS_HTTP_HUB_TAG} as neofs-http-gw
+FROM ${NEOFS_HUB_IMAGE}-rest-gw:${NEOFS_REST_HUB_TAG} as neofs-rest-gw
 
 # Executable image
 FROM alpine AS neofs-aio
@@ -13,13 +20,33 @@ RUN apk add --no-cache \
   ca-certificates \
   jq \
   expect \
-  iputils
+  iputils \
+  curl
 
 WORKDIR /
 
-COPY --from=neofs-cli /bin/neofs-cli /bin/neofs-cli
-COPY --from=neofs-ir /bin/neofs-ir /bin/neofs-ir
-COPY --from=neofs-storage /bin/neofs-node /bin/neofs-node
-COPY --from=neofs-adm /bin/neofs-adm /bin/neofs-adm
+COPY --from=neo-go /usr/bin/privnet-entrypoint.sh /usr/bin/privnet-entrypoint.sh
+COPY --from=neo-go /etc/ssl/certs /etc/ssl/certs
+COPY --from=neo-go /usr/bin/neo-go /usr/bin/neo-go
+COPY --from=neofs-cli /bin/neofs-cli /usr/bin/neofs-cli
+COPY --from=neofs-ir /bin/neofs-ir /usr/bin/neofs-ir
+COPY --from=neofs-storage /bin/neofs-node /usr/bin/neofs-node
+COPY --from=neofs-adm /bin/neofs-adm /usr/bin/neofs-adm
+COPY --from=neofs-http-gw /bin/neofs-http-gw /usr/bin/neofs-http-gw
+COPY --from=neofs-rest-gw /bin/neofs-rest-gw /usr/bin/neofs-rest-gw
 
-CMD ["neofs-cli"]
+COPY ./sn/cli-cfg.yaml /config/cli-cfg-sn.yaml
+COPY ./sn/wallet.json /config/wallet-sn.json
+COPY ./sn/config.yaml /config/config-sn.yaml
+COPY ./http/wallet.json /config/wallet-http.json
+COPY ./rest-gw/wallet.json /config/wallet-rest.json
+COPY ./ir/cli-cfg.yaml /config/cli-cfg-ir.yaml
+COPY ./ir/config.yaml /config/config-ir.yaml
+COPY ./vendor/locode_db /config/locode.db
+COPY ./vendor/morph_chain.gz /config/morph_chain.gz
+COPY ./morph/protocol.privnet.yml /config/protocol.privnet.yml
+COPY ./morph/node-wallet.json /config/node-wallet.json
+COPY ./morph/node-config.yaml /config/node-config.yaml
+COPY ./bin/ /config/bin
+
+ENTRYPOINT ["/config/bin/init-aio.sh"]

Makefile
@@ -8,42 +8,33 @@ REPO=$(notdir $(shell pwd))
 VERSION ?= "$(shell git describe --tags --match "v*" 2>/dev/null || git rev-parse --short HEAD | sed 's/^v//')"
 
 # Variables for docker
-HUB_IMAGE ?= "nspccdev/neofs"
-AIO_IMAGE ?= "$(HUB_IMAGE)-aio"
-TESTCONTAINER_IMAGE ?= "$(AIO_IMAGE)-testcontainer"
+NEOGO_HUB_IMAGE ?= "nspccdev/neo-go"
+NEOFS_HUB_IMAGE ?= "nspccdev/neofs"
+AIO_IMAGE ?= "$(NEOFS_HUB_IMAGE)-aio"
 
 # Build aio Docker image
 image-aio:
 	@echo "⇒ Build aio docker image "
 	@docker build \
 		--rm \
-		--build-arg HUB_IMAGE=$(HUB_IMAGE) \
-        --build-arg HUB_TAG=$(AIO_VERSION) \
+		--build-arg NEOFS_HUB_IMAGE=$(NEOFS_HUB_IMAGE) \
+        --build-arg NEOFS_HUB_TAG=$(AIO_VERSION) \
+		--build-arg NEOGO_HUB_IMAGE=$(NEOGO_HUB_IMAGE) \
+        --build-arg NEOGO_HUB_TAG=$(NEOGO_VERSION) \
+        --build-arg NEOFS_HTTP_HUB_TAG=$(HTTPGW_VERSION) \
+        --build-arg NEOFS_REST_HUB_TAG=$(RESTGW_VERSION) \
 		-f Dockerfile \
 		-t $(AIO_IMAGE):$(AIO_VERSION) .
-
-# Build testcontainer Docker image
-image-testcontainer: image-aio
-	@echo "⇒ Build testcontainer docker image "
-	@docker build \
-		--rm \
-		--build-arg NEOGO_HUB_TAG=$(NEOGO_VERSION) \
-        --build-arg AIO_HUB_TAG=$(AIO_VERSION) \
-		-f testcontainer/Dockerfile \
-		-t $(TESTCONTAINER_IMAGE):$(AIO_VERSION) .
-
 
 # Tick new epoch in side chain
 tick.epoch:
 	@./bin/tick.sh
 
-# Update container fee per alphabet node
-prepare.ir:
-	@./bin/config.sh ContainerFee 0
-	@./bin/config.sh ContainerAliasFee 0
-
 # Show current version
 version:
 	@echo $(VERSION)
 	@echo "neofs-node: $(AIO_VERSION)"
+	@echo "neo-go: $(NEOGO_VERSION)"
+	@echo "neofs-http-gw: $(HTTPGW_VERSION)"
+	@echo "neofs-rest-gw: $(RESTGW_VERSION)"
 

bin/config.sh
@@ -1,17 +1,14 @@
 #!/usr/bin/env bash
 
 # NeoGo binary path.
-NEOGO="${NEOGO:-docker exec -it morph neo-go}"
+NEOGO="${NEOGO:-docker exec aio neo-go}"
 
-# Wallet files to change config value
-WALLET="${WALLET:-./morph/node-wallet.json}"
-WALLET_IMG="${WALLET_IMG:-config/node-wallet.json}"
+# Wallet file to change config value
+WALLET="${WALLET:-morph/node-wallet.json}"
+CONFIG_IMG="${CONFIG:-/config/node-config.yaml}"
 
 # Netmap contract address resolved by NNS
-NETMAP_ADDR=`./bin/resolve.sh netmap.neofs`
-
-# Wallet password that would be entered automatically; '-' means no password
-PASSWD="one"
+NETMAP_ADDR=$(./bin/resolve.sh netmap.neofs) || die "Failed to resolve 'netmap.neofs' domain name"
 
 # NeoFS configuration record: key is a string and value is an int
 KEY=${1}
@@ -21,18 +18,18 @@ VALUE="${2}"
 [ -z "$VALUE" ] && echo "Empty config value" && exit 1
 
 # Internal variables
-ADDR=`cat ${WALLET} | jq -r .accounts[2].address`
+ADDR=$(jq -r .accounts[2].address "${WALLET}")
 
 # Change config value in side chain
-echo "Changing ${KEY} configration value to ${VALUE}"
-./bin/passwd.exp ${PASSWD} ${NEOGO} contract invokefunction \
--w ${WALLET_IMG} \
--a ${ADDR} \
--r http://localhost:30333 \
-${NETMAP_ADDR} \
-setConfig bytes:beefcafe \
-string:${KEY} \
-int:${VALUE} -- ${ADDR} || exit 1
+echo "Changing ${KEY} configuration value to ${VALUE}"
+${NEOGO} contract invokefunction \
+	--wallet-config "${CONFIG_IMG}" \
+  -a "${ADDR}" --force \
+  -r http://localhost:30333 \
+  "${NETMAP_ADDR}" \
+  setConfig bytes:beefcafe \
+  string:"${KEY}" \
+  int:"${VALUE}" -- "${ADDR}" || exit 1
 
 # Update epoch to apply new configuration value
 ./bin/tick.sh

bin/passwd.exp
@@ -1,23 +1,0 @@
-#!/usr/bin/expect
-
-set passwd [lindex $argv 0]
-set args [lrange $argv 1 end]
-
-spawn -noecho {*}$args
-expect -re {^.*assword.*$}
-
-if { $passwd == "-"} {
-	send -- "\r"
-} else {
-	send -- "$passwd\r"
-}
-
-expect {
-    "Relay transaction" {
-        send "y\r"
-        exp_continue
-    }
-    EOF
-}
-lassign [wait] pid spawnid os_error_flag value
-exit $value

bin/resolve.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 # NeoGo binary path.
-NEOGO="${NEOGO:-docker exec -it morph neo-go}"
+NEOGO="${NEOGO:-docker exec aio neo-go}"
 # NNS contract script hash
-NNS_ADDR=`curl -s --data '{ "id": 1, "jsonrpc": "2.0", "method": "getcontractstate", "params": [1] }' http://localhost:30333/ | jq -r '.result.hash'`
+NNS_ADDR=$(curl -s --data '{ "id": 1, "jsonrpc": "2.0", "method": "getcontractstate", "params": [1] }' http://localhost:30333/ | jq -r '.result.hash')
 
 ${NEOGO} contract testinvokefunction \
--r http://localhost:30333 \
-${NNS_ADDR} resolve string:${1} int:16 | jq -r '.stack[0].value | if type=="array" then .[0].value else . end' | base64 -d
+  -r http://localhost:30333 \
+  "${NNS_ADDR}" resolve string:"${1}" int:16 | jq -r '.stack[0].value | if type=="array" then .[0].value else . end' | base64 -d

bin/tick.sh
@@ -1,31 +1,28 @@
 #!/usr/bin/env bash
 
 # NeoGo binary path.
-NEOGO="${NEOGO:-docker exec -it morph neo-go}"
+NEOGO="${NEOGO:-docker exec aio neo-go}"
 
-# Wallet files to change config value
+# Wallet file to change config value
 WALLET="${WALLET:-./morph/node-wallet.json}"
-WALLET_IMG="${WALLET_IMG:-config/node-wallet.json}"
+CONFIG_IMG="${CONFIG:-/config/node-config.yaml}"
 
 # Netmap contract address resolved by NNS
-NETMAP_ADDR=`./bin/resolve.sh netmap.neofs`
-
-# Wallet password that would be entered automatically; '-' means no password
-PASSWD="one"
+NETMAP_ADDR=$(./bin/resolve.sh netmap.neofs)
 
 # Internal variables
-ADDR=`cat ${WALLET} | jq -r .accounts[2].address`
+ADDR=$(jq -r .accounts[2].address "${WALLET}")
 
 # Fetch current epoch value
-EPOCH=`${NEOGO} contract testinvokefunction -r \
-http://localhost:30333 \
-${NETMAP_ADDR} \
-epoch | grep 'value' | awk -F'"' '{ print $4 }'`
+EPOCH=$(${NEOGO} contract testinvokefunction -r \
+  http://localhost:30333 \
+  "${NETMAP_ADDR}" \
+  epoch | grep 'value' | awk -F'"' '{ print $4 }')
 
 echo "Updating NeoFS epoch to $((EPOCH+1))"
-./bin/passwd.exp ${PASSWD} ${NEOGO} contract invokefunction \
--w ${WALLET_IMG} \
--a ${ADDR} \
--r http://localhost:30333 \
-${NETMAP_ADDR} \
-newEpoch int:$((EPOCH+1)) -- ${ADDR}:Global
+${NEOGO} contract invokefunction \
+  --wallet-config "${CONFIG_IMG}" \
+  -a "${ADDR}" --force \
+  -r http://localhost:30333 \
+  "${NETMAP_ADDR}" \
+  newEpoch int:$((EPOCH+1)) -- "${ADDR}":Global

docker-compose.cross.yml
@@ -1,139 +1,0 @@
----
-
-version: "2.4"
-services:
-  morph:
-    image: nspccdev/neo-go:${NEOGO_VERSION}
-    container_name: morph
-    domainname: neofs
-    hostname: morph
-    restart: always
-    stop_signal: SIGKILL
-    environment:
-      - ACC=/config/morph_chain.gz
-    volumes:
-      - ./vendor/morph_chain.gz:/config/morph_chain.gz
-      - ./morph/protocol.privnet.yml:/config/protocol.privnet.yml
-      - ./morph/node-wallet.json:/config/node-wallet.json
-    ports:
-      - "30333:30333" # RPC
-    healthcheck:
-      test: [ "CMD-SHELL", "neo-go query committee -r http://127.0.0.1:30333 | grep 02b3622bf4017bdfe317c58aed5f4c753f206b7db896046fa7d774bbc4bf7f8dc2" ]
-      interval: 2s
-      timeout: 3s
-      retries: 5
-      start_period: 10s
-
-  ir:
-    image: nspccdev/neofs-aio:${AIO_VERSION}
-    container_name: ir
-    domainname: neofs
-    hostname: ir
-    restart: on-failure
-    stop_signal: SIGKILL
-    environment:
-      - NEOFS_IR_MORPH_ENDPOINT_CLIENT_0_ADDRESS=ws://morph:30333/ws
-    volumes:
-      - ./morph/node-wallet.json:/config/node-wallet.json
-      - ./ir/cli-cfg.yaml:/config/cli-cfg.yaml
-      - ./ir/config.yaml:/config/config.yaml
-      - ./vendor/locode_db:/config/locode.db
-    entrypoint: /bin/neofs-ir
-    command: --config /config/config.yaml
-    healthcheck:
-      test: [ "CMD-SHELL", "/bin/neofs-cli control healthcheck --ir --endpoint localhost:16512 -c /config/cli-cfg.yaml | grep 'Health status: READY'" ]
-      interval: 2s
-      timeout: 3s
-      retries: 5
-      start_period: 20s
-    depends_on:
-      morph:
-        condition: service_healthy
-
-  sn:
-    image: nspccdev/neofs-aio:${AIO_VERSION}
-    domainname: neofs
-    hostname: sn
-    container_name: sn
-    restart: on-failure
-    stop_signal: SIGKILL
-    environment:
-      - NEOFS_MORPH_RPC_ENDPOINT_0_ADDRESS=ws://morph:30333/ws
-      - NEOFS_NODE_ADDRESSES=grpc://sn:8080
-      - NEOFS_GRPC_0_ENDPOINT=:8080
-      - NEOFS_CONTROL_GRPC_ENDPOINT=:16513
-    volumes:
-      - ./sn/cli-cfg.yaml:/config/cli-cfg.yaml
-      - ./sn/wallet.json:/config/wallet-sn.json
-      - ./sn/config.yaml:/config/config.yaml
-      - data:/data
-    ports:
-      - "8080:8080"   # NeoFS API RPC
-      - "16513:16513" # Control service
-    entrypoint: /bin/neofs-node
-    command: --config /config/config.yaml
-    healthcheck:
-      test: [ "CMD-SHELL", "/bin/neofs-cli control healthcheck --endpoint localhost:16513 -c /config/cli-cfg.yaml | grep 'Health status: READY'" ]
-      interval: 2s
-      timeout: 3s
-      retries: 5
-      start_period: 10s
-    depends_on:
-      ir:
-        condition: service_healthy
-
-  http_gw:
-    image: nspccdev/neofs-http-gw:${HTTPGW_VERSION}
-    domainname: neofs
-    hostname: http_gw
-    container_name: http_gw
-    restart: on-failure
-    stop_signal: SIGKILL
-    env_file: [ "./http/http.env" ]
-    environment:
-      - HTTP_GW_LISTEN_ADDRESS=:8081
-      - HTTP_GW_PEERS_0_ADDRESS=sn:8080
-      - HTTP_GW_RPC_ENDPOINT=http://morph:30333
-    volumes:
-      - ./http/wallet.json:/config/wallet.json
-    ports:
-      - "8081:8081" # HTTP Gateway endpoint
-    depends_on:
-      - sn
-
-  rest_gw:
-    image: nspccdev/neofs-rest-gw:${RESTGW_VERSION}
-    domainname: neofs
-    hostname: rest_gw
-    container_name: rest_gw
-    restart: on-failure
-    stop_signal: SIGKILL
-    env_file: [ "./rest-gw/rest.env" ]
-    environment:
-      - REST_GW_LISTEN_ADDRESS=:8090
-      - REST_GW_PEERS_0_ADDRESS=sn:8080
-    volumes:
-      - ./rest-gw/node-wallet.json:/config/wallet.json
-    ports:
-      - "8090:8090" # REST Gateway endpoint
-    depends_on:
-      - sn
-
-  nginx_gw:
-    image: nginx:stable-alpine
-    domainname: neofs
-    hostname: nginx_gw
-    container_name: nginx_gw
-    restart: on-failure
-    stop_signal: SIGKILL
-    volumes:
-      - ./http/nginx.cross.conf:/etc/nginx/nginx.conf
-      - cache:/cache
-    ports:
-      - "8082:8082" # nginx gateway endpoint
-    depends_on:
-      - http_gw
-
-volumes:
-  data:
-  cache:

commit 7b3f02ad7da0810022034b6b135d6e9c7f61355b

bin: wait for SN to be online before declaring OK state
Fixes #54. Regression since 74b925abde8a96ea22882d73b1ad797d30d7d5fb.

Signed-off-by: Roman Khimov <roman@nspcc.ru>

bin/init-aio.sh
@@ -24,7 +24,7 @@ done
 set -m
 /usr/bin/neofs-node --config /config/config-sn.yaml &
 
-while [[ -z "$(/usr/bin/neofs-cli control healthcheck --endpoint localhost:16513 -c /config/cli-cfg-sn.yaml | grep 'Health status: READY')" ]];
+while [[ -z "$(/usr/bin/neofs-cli control healthcheck --endpoint localhost:16513 -c /config/cli-cfg-sn.yaml | grep 'Network status: ONLINE')" ]];
 do
   ./bin/tick.sh
   sleep 5;